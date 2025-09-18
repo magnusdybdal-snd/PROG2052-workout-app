d62877942582ca1af3c2f4e515c7047a32d3a1c3
maeilert <maeilert@stud.ntnu.no>
2025-09-18 13:32:09 +0200

    push-pop navigation for everything

---
+++ frontend/app/src/main/java/com/example/workoutapp/Routes.kt
@@ -0,0 +1,8 @@
+package com.example.workoutapp
+
+object Routes {
+    const val HISTORY = "history"
+    const val WORKOUT = "workout"
+    const val EXERCISES = "exercises"
+    const val TEST = "test" // TODO remove test
+}
