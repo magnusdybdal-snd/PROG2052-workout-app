13a30b5ac3c3c7939001f8c0df6ed59e15e251fa
maeilert <maeilert@stud.ntnu.no>
2025-09-18 15:52:45 +0200

    HistoryPage built and styled.

--- frontend/app/src/main/java/com/example/workoutapp/pages/HistoryPage.kt
+++ frontend/app/src/main/java/com/example/workoutapp/pages/HistoryPage.kt
@@ -4,14 +4,19 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -22,6 +27,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.workoutapp.R
@@ -35,6 +41,7 @@ fun HistoryPage(modifier: Modifier = Modifier) {
         modifier = Modifier
             .fillMaxWidth()
             .widthIn(max = 500.dp)
+            .padding(bottom = 80.dp) // padding to compensate for navbar - navigationBarsPadding()?
             .background(Color.White),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -51,59 +58,79 @@ fun HistoryPage(modifier: Modifier = Modifier) {
 
     Column( // Boxes
        modifier = Modifier
-            .fillMaxSize()
-            .widthIn(max = 500.dp)
-            .background(Color.White),
+           .widthIn(max = 700.dp)
+           .background(Color.White)
+           .padding(bottom = 20.dp)
+           .verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.Center,
+
 //        horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        for (i in 1..3){ // loop over months
         Text( // Month
-            modifier = Modifier.padding(start = 20.dp),
+            modifier = Modifier.padding(start = 20.dp, top = 10.dp),
             text = "September", // Value hardcoded until backend Logic is available
             fontSize = 20.sp,
-            //fontWeight = FontWeight.SemiBold,
+            fontWeight = FontWeight.SemiBold,
 
         )
-        Row(
-            modifier = Modifier
-                .heightIn(80.dp, 80.dp)
-                .padding(vertical = 6.dp)
-                .border(width = 2.dp, color = Color.Black),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-
-
-                Column(
-                    modifier = Modifier.padding(start = 30.dp)
-                ){
-                    Text( // Workout name
-                        text = "My Workout x ",
-                        fontSize = 20.sp
-                    )
-                    Row {
-                        Icon(
-                            imageVector = Icons.Default.Info,
-                            contentDescription = "",
+        // Looping for more elements.
+        for (i in 1..3) {
+            Box( // vertical space between boxes
+                modifier = Modifier.padding(6.dp)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 20.dp)
+                        .border(width = 2.dp, color = Color.Black)
+                        .padding(vertical = 10.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Column(
+                        modifier = Modifier.padding(start = 30.dp)
+                    ) {
+                        Text( // Workout name
+                            text = "My Workout $i ",
+                            fontSize = 20.sp
                         )
-                        Text(
-                            text = "1:29:29",
-                            modifier.padding(start = 6.dp)
-                        )
+                        Row {
+                            Icon(
+                                imageVector = Icons.Default.Info,
+                                contentDescription = "",
+                            )
+                            Text(
+                                text = "1:29:29",
+                                modifier.padding(start = 6.dp)
+                            )
+                            Row(
+                                modifier = Modifier.padding(start = 10.dp)
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.AccountCircle,
+                                    contentDescription = "",
+                                )
+                                Text(
+                                    text = "1234 kg",
+                                    modifier.padding(start = 6.dp)
+                                )
 
-                    }
+                            }
+                        }
 
-                } // end column 1 "woukout text
-                Column ( // Date
-                    horizontalAlignment = Alignment.End,
-                    verticalArrangement = Arrangement.Center
-                ){
+                    } // end column 1 "workout text
                     Text(
-                       modifier = Modifier.padding(end = 40.dp),
-                        text = "29.29.2929",
-                        )
+                        modifier = Modifier.padding(end = 30.dp),
+                        text = "29.29.2029",
+                        textAlign = TextAlign.End
+                    )
                 }
+            }
 
         }
     }
+    }
 }
 }
+
