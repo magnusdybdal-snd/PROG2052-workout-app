d3d3e3023be1a04fe00634487acc1c87e3f80aec
maeilert <maeilert@stud.ntnu.no>
2025-10-09 16:09:41 +0200

    stremline ExerciseColor color

--- frontend/app/src/main/java/com/example/workoutapp/features/exercises/ExercisesPage.kt
+++ frontend/app/src/main/java/com/example/workoutapp/features/exercises/ExercisesPage.kt
@@ -14,13 +14,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -48,7 +48,7 @@ fun ExercisesPage(
                 modifier = Modifier
                     .fillMaxSize()
                     .widthIn(max = 700.dp)
-                    .background(Color.White),
+                    .background(MaterialTheme.colorScheme.background),
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
@@ -58,7 +58,7 @@ fun ExercisesPage(
                         .padding(bottom = 20.dp),
                     text = "Exercises",
                     fontSize = 50.sp,
-                    color = Color.Black,
+                    color = MaterialTheme.colorScheme.onBackground,
                 )
                 Column( // All exercises
                     modifier = modifier
@@ -75,7 +75,7 @@ fun ExercisesPage(
                                 .fillMaxWidth()
                                 .heightIn(80.dp, 80.dp)
                                 .padding(vertical = 6.dp)
-                                .border(width = 2.dp, color = Color.Black),
+                                .border(width = 2.dp, color = MaterialTheme.colorScheme.onBackground),
                             verticalAlignment = Alignment.CenterVertically
                         ) {
                             Image( // Picture of exercise
