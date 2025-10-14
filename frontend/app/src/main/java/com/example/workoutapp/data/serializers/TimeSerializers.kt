944e45556a7eb9aea0b140633478747ec1577495
Magnus Dybdal <magndy@stud.ntnu.no>
2025-10-14 13:44:35 +0200

    added string parsers for Duration and LocalTime

--- frontend/app/src/main/java/com/example/workoutapp/data/serializers/TimeSerializers.kt
+++ frontend/app/src/main/java/com/example/workoutapp/data/serializers/TimeSerializers.kt
@@ -1,2 +1,48 @@
 package com.example.workoutapp.data.serializers
 
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import java.io.Serial
+import java.time.Duration
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+
+/**
+ * Serializer for LocalDate using ISO-8601 format ("2025-10-09")
+ */
+object LocalDataSerializer: KSerializer<LocalDate> {
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE
+
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("LocalDate", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: LocalDate) {
+        encoder.encodeString(value.format(formatter))
+    }
+
+    override fun deserialize(decoder: Decoder): LocalDate {
+        return LocalDate.parse(decoder.decodeString(), formatter)
+    }
+}
+
+/**
+ * Serializer for Duration using ISO-8601 format ("PT1H15M45S")
+ */
+object DurationSerializer: KSerializer<Duration> {
+
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("Duration", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: Duration) {
+        encoder.encodeString(value.toString())
+    }
+
+    override fun deserialize(decoder: Decoder): Duration {
+        return Duration.parse(decoder.decodeString())
+    }
+}
