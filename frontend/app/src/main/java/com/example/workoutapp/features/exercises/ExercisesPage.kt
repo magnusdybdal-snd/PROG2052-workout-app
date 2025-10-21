29d4863e9abccc1aa79f7f559ba530a5cfe9948b
Jonas <jonnyga@ntnu.no>
2025-10-21 13:12:59 +0200

    Search for exercise

--- frontend/app/src/main/java/com/example/workoutapp/features/exercises/ExercisesPage.kt
+++ frontend/app/src/main/java/com/example/workoutapp/features/exercises/ExercisesPage.kt
@@ -2,14 +2,28 @@ package com.example.workoutapp.features.exercises
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.example.workoutapp.R
@@ -33,26 +47,48 @@ fun ExercisesPage(
     viewModel: ExercisesViewModel = hiltViewModel()
 ) {
     val state by viewModel.uiState.collectAsState()
+    val cs = MaterialTheme.colorScheme
 
     when {
         state.isLoading -> LoadingStateView()
         state.error != null -> ErrorStateView(state.error)
         else -> {
+            var searchString by remember { mutableStateOf("") }
+
             Column(
                 modifier = PageColumnModifier(),
-                verticalArrangement = Arrangement.Center,
+                verticalArrangement = Arrangement.Top,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 PageHeading(
                     displayText = stringResource(R.string.exercises)
                 )
+
+                TextField(
+                    value = searchString,
+                    onValueChange = { searchString = it },
+                    label = { Text(
+                        "Enter something",
+                        color = cs.onBackground
+                    ) },
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .fillMaxWidth()
+                )
+
                 Column( // All exercises
                     modifier = Modifier.verticalScroll(rememberScrollState()),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     state.exercises.forEach { exercise: Exercise ->
-                        ExerciseDisplayBox(exercise)
+                        if(searchString != "") {
+                            if (exercise.name.contains(searchString)) {
+                                ExerciseDisplayBox(exercise)
+                            }
+                        } else {
+                            ExerciseDisplayBox(exercise)
+                        }
                     }
                 }
             }
