b9c6000474d35ac7e10fabe5b2b7a7f4e177819f
maeilert <maeilert@stud.ntnu.no>
2025-10-30 18:05:06 +0100

    removed unused imports

--- frontend/app/src/main/java/com/example/workoutapp/core/core_ui/composable/ExerciseDetailPage.kt
+++ frontend/app/src/main/java/com/example/workoutapp/core/core_ui/composable/ExerciseDetailPage.kt
@@ -1,17 +1,10 @@
 package com.example.workoutapp.core.core_ui.composable
 
-import com.example.workoutapp.R
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.animation.slideIn
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -23,18 +16,13 @@ import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.zIndex
 import com.example.workoutapp.domain.models.Exercise
-import java.lang.System.exit
 
 @Composable
 fun ExerciseDetailPage(
