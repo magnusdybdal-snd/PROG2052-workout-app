7e9d67f2a81b2a11404fe2d6a45ae440d3df46bd
Magnus Dybdal <magndy@stud.ntnu.no>
2025-10-30 21:19:30 +0100

    implemeted editing of template to room + confirmation message

--- frontend/app/src/main/java/com/example/workoutapp/features/edit_template/EditTemplateViewModel.kt
+++ frontend/app/src/main/java/com/example/workoutapp/features/edit_template/EditTemplateViewModel.kt
@@ -5,24 +5,19 @@ import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.workoutapp.domain.models.Exercise
-import com.example.workoutapp.domain.models.Session
 import com.example.workoutapp.domain.models.WorkoutTemplate
 import com.example.workoutapp.domain.usecases.EditWorkoutTemplateUseCase
 import com.example.workoutapp.domain.usecases.GetExercisesUseCase
 import com.example.workoutapp.domain.usecases.GetWorkoutTemplatesUseCase
-import com.example.workoutapp.domain.usecases.PostHistoryWorkoutUseCase
-import com.example.workoutapp.features.exercises.ExercisesUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import java.time.LocalTime
-import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 // UI state holder: represents what's shown on the "Active workout" screen.
-data class ActiveWorkoutUiState(
+data class EditTemplateUiState(
     val isLoading: Boolean = false,
     val templates: List<WorkoutTemplate> = emptyList(),
     val exercises: List<Exercise> = emptyList(), // add this
@@ -32,16 +27,16 @@ data class ActiveWorkoutUiState(
 // @HiltViewModel: tells Hilt this ViewModel can have dependencies injected.
 // Hilt will generate all the factory code needed to create it.
 @HiltViewModel
-class ActWorkViewModel @Inject constructor(  // @Inject = Hilt can construct this
+class EditTemplateViewModel @Inject constructor(  // @Inject = Hilt can construct this
     private val getWorkoutTemplatesUseCase: GetWorkoutTemplatesUseCase,
     private val editWorkoutTemplatesUseCase: EditWorkoutTemplateUseCase,
     private val getExercisesUseCase: GetExercisesUseCase
 ) : ViewModel() {
 
     // The viewmodel can change this instance
-    private val _uiState = MutableStateFlow(ActiveWorkoutUiState())
+    private val _uiState = MutableStateFlow(EditTemplateUiState())
     // This immutable instance is for the UI, read only
-    val uiState: StateFlow<ActiveWorkoutUiState> = _uiState
+    val uiState: StateFlow<EditTemplateUiState> = _uiState
 
     // Runs once when the class is instantiated
     init {
@@ -64,8 +59,8 @@ class ActWorkViewModel @Inject constructor(  // @Inject = Hilt can construct thi
                         )
                     }
 
-                    _uiState.value = ActiveWorkoutUiState(
-                        templates = statefulTemplates.sortedBy { it.createdAt },
+                    _uiState.value = EditTemplateUiState(
+                        templates = statefulTemplates.sortedByDescending { it.createdAt },
                         exercises = _uiState.value.exercises,
                         isLoading = false
                     )
@@ -81,7 +76,7 @@ class ActWorkViewModel @Inject constructor(  // @Inject = Hilt can construct thi
             _uiState.update { it.copy(isLoading = true) }
             try {
                 val data = getExercisesUseCase()
-                Log.d("ExercisesViewModel", "Fetched ${data.size} exercises")
+                Log.d("EditTemplateViewModel", "Fetched ${data.size} exercises")
                 _uiState.update { current ->
                     current.copy(
                         exercises = data.sortedBy { it.name.lowercase() },
@@ -98,15 +93,6 @@ class ActWorkViewModel @Inject constructor(  // @Inject = Hilt can construct thi
         viewModelScope.launch {
             try {
                 editWorkoutTemplatesUseCase(workoutTemplate)
-
-                _uiState.update { current ->
-                    val editedTemplates = current.templates.toMutableList()
-                    val index = editedTemplates.indexOfFirst { it.templateId == workoutTemplate.templateId }
-                    if (index != -1) {
-                        editedTemplates[index] = workoutTemplate
-                    }
-                    current.copy(templates = editedTemplates)
-                }
             } catch (e: Exception) {
                 _uiState.update { it.copy(error = e.message ?: "Failed to edit template") }
             }
