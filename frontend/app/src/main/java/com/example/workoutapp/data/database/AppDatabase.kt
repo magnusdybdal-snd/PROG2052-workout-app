d7fa9c85be4f3fc557d82d1a68a08d8cb5efc762
Magnus Dybdal <magndy@stud.ntnu.no>
2025-10-31 14:58:50 +0100

    updated repo interface to implement syncDelete and edit

--- frontend/app/src/main/java/com/example/workoutapp/data/database/AppDatabase.kt
+++ frontend/app/src/main/java/com/example/workoutapp/data/database/AppDatabase.kt
@@ -29,7 +29,7 @@ import com.example.workoutapp.data.database.entities.templates.TemplateSetEntity
         TemplateExerciseEntity::class,
         TemplateSetEntity::class
        ],
-    version = 10,
+    version = 11,
     exportSchema = false
 )
 // Needs a converter as Room does not support Duration and LocalDate
--- frontend/app/src/main/java/com/example/workoutapp/domain/repositories/HistoryWorkoutRepository.kt
+++ frontend/app/src/main/java/com/example/workoutapp/domain/repositories/HistoryWorkoutRepository.kt
@@ -37,4 +37,8 @@ interface HistoryWorkoutRepository {
      * Deletes a workout to the API and removes it locally.
      */
     suspend fun deleteHistoryWorkout(historyWorkout: HistoryWorkout)
+
+    suspend fun syncDeleteTemplates()
+
+    suspend fun editHistoryWorkout(historyWorkout: HistoryWorkout)
 }
