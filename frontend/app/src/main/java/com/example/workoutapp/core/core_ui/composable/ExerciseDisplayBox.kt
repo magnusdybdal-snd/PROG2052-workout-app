c14dfbd04fdcf56fdf89ef0fd302f5ef3166cfeb
maeilert <maeilert@stud.ntnu.no>
2025-11-16 12:24:56 +0100

    placeholder image when gif-loading error

--- frontend/app/src/main/java/com/example/workoutapp/core/core_ui/composable/ExerciseDisplayBox.kt
+++ frontend/app/src/main/java/com/example/workoutapp/core/core_ui/composable/ExerciseDisplayBox.kt
@@ -10,10 +10,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.imageLoader
+import coil.request.ImageRequest
+import com.example.workoutapp.R
 import com.example.workoutapp.core.core_ui.composable.modifiers.BorderBoxModifier
 import com.example.workoutapp.domain.models.Exercise
 
@@ -30,17 +33,21 @@ fun ExerciseDisplayBox(
         modifier = BorderBoxModifier(),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        val imageLoader = LocalContext.current.imageLoader
+        val context = LocalContext.current
+        val imageLoader = context.imageLoader
 
         AsyncImage(
-            model = exercise.gifUrl,
+            model = ImageRequest.Builder(context)
+                .data(exercise.gifUrl)
+                .crossfade(true)
+                .build(),
             imageLoader = imageLoader,
             contentDescription = "Exercise demonstration",
             contentScale = ContentScale.Inside,
-            modifier = Modifier.padding(vertical = 8.dp) // Padding between border and image.
-
+            modifier = Modifier.padding(vertical = 8.dp),
+            error = painterResource(R.drawable.exampleworkoutimage), // If load fails
         )
-        Text( // Exercise name
+        Text(
             modifier = Modifier.padding(6.dp),
             text = exercise.name,
             fontSize = 20.sp
