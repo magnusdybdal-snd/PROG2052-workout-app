b7a6fb42c8abd4c30d7e821abfdb0406fd930adb
Magnus Dybdal <magndy@stud.ntnu.no>
2025-11-25 16:15:53 +0100

    made thumbnail detail pic when offline

--- frontend/app/src/main/java/com/example/workoutapp/features/exercise_detail/ExerciseDetailPage.kt
+++ frontend/app/src/main/java/com/example/workoutapp/features/exercise_detail/ExerciseDetailPage.kt
@@ -22,13 +22,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import coil.decode.ImageDecoderDecoder
 import coil.request.ImageRequest
+import coil.compose.rememberAsyncImagePainter
+import coil.imageLoader
+import com.example.workoutapp.R
 import com.example.workoutapp.domain.models.Exercise
 
 @RequiresApi(Build.VERSION_CODES.P)
@@ -85,13 +90,28 @@ fun ExerciseDetailPage(
                         lineHeight = 30.sp,
                         color = cs.onBackground,
                     )
+                    val context = LocalContext.current
+
                     AsyncImage(
                         model = ImageRequest.Builder(LocalContext.current)
                             .data(exercise.gifUrl)
                             .decoderFactory(ImageDecoderDecoder.Factory()) // TODO ensure SDK26 support (current SDK > 28)
                             .build(),
+                        imageLoader = context.imageLoader,
                         contentDescription = "Exercise demonstration",
-                        modifier = Modifier.size(200.dp)
+                        contentScale = ContentScale.Fit,
+                        modifier = Modifier
+                            .size(200.dp)
+                            .align(Alignment.CenterHorizontally)
+                            .padding(horizontal = 20.dp),
+                        placeholder = rememberAsyncImagePainter(
+                            model = exercise.getLocalThumbnailPath(),
+                            imageLoader = context.imageLoader
+                        ),
+                        error = rememberAsyncImagePainter(
+                            model = exercise.getLocalThumbnailPath(),
+                            imageLoader = context.imageLoader
+                        )
                     )
 
                     // --- Details ---
